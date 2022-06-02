1d741b8e599d56b4af23e4d396fa733c909db6c6
zhhanng <2585518097@qq.com>
2022-06-02 09:23:58 +0800

    新建

---
+++ src/main/java/com/atguigu/Demo1.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * Date:2022/6/2
+ * Author:zh
+ * Description:
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("新建测试");
+    }
+}
