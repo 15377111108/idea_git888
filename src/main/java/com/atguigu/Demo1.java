be3a9b52f7fa25cac2fff377b1ea432d0b0793d4
zhhanng <2585518097@qq.com>
2022-06-02 10:21:12 +0800

    冲突测试

--- src/main/java/com/atguigu/Demo1.java
+++ src/main/java/com/atguigu/Demo1.java
@@ -8,6 +8,7 @@ package com.atguigu;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("新建测试");
+        System.out.println("第一次更新");
         System.out.println("master添加测试");
     }
 }
